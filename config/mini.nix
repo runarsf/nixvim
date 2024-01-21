c6bc3aeb147b5b5aa036d090edcd5ee8a03a5c6a
Runar Fredagsvik <i@runar.ch>
2024-01-21 23:14:48 +0100

    update mini.files binds

--- config/mini.nix
+++ config/mini.nix
@@ -10,12 +10,12 @@
       bufremove = { };
       # tabline = { };
       files = {
+        # TODO Multiple mappings https://github.com/echasnovski/mini.nvim/discussions/409
+        #  Enter / e for go_in
         mappings = {
-          go_in       = "<Right>";
-          go_in_plus  = "<S-Right>";
-          go_out      = "<Left>";
-          go_out_plus = "<S-Left>";
-          synchronize = "R";
+          go_in_plus  = "<Right>";
+          go_out_plus = "<Left>";
+          synchronize = "W";
         };
       };
     };
