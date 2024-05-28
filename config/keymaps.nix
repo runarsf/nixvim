e07ac4f8438778e872f682d248227514920bb103
Runar Fredagsvik <i@runar.ch>
2024-05-28 16:53:58 +0200

    navigate wrapped lines

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -24,6 +24,31 @@
     maplocalleader = "<Space>";
   };
   keymaps = [
+    { # https://stackoverflow.com/a/21000307
+      key = "<up>";
+      action = "(v:count == 0 ? 'gk' : 'k')";
+      mode = [ "n" ];
+      options = {
+        desc = "Move cursor up (including wrapped lines)";
+        noremap = true;
+        expr = true;
+        nowait = true;
+        silent = true;
+      };
+    }
+    {
+      key = "<down>";
+      action = "(v:count == 0 ? 'gj' : 'j')";
+      mode = [ "n" ];
+      options = {
+        desc = "Move cursor down (including wrapped lines)";
+        noremap = true;
+        expr = true;
+        nowait = true;
+        silent = true;
+      };
+    }
+
     {
       key = "<leader><Space>";
       action = "<CMD>nohlsearch<CR>";
