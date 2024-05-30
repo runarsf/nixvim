fc5683fd0d8417ec560dce21f3a1020c6ccfe201
Runar Fredagsvik <i@runar.ch>
2024-05-30 21:57:22 +0200

    add formatters

--- config/conform.nix
+++ config/conform.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+_:
 
 {
   keymaps = [{
@@ -9,12 +9,25 @@
 
   plugins.conform-nvim = {
     enable = true;
-    formatters = {
-      nixfmt = {
-        command = "${pkgs.nixfmt}/bin/nixfmt";
-        args = [ "$FILENAME" ];
-      };
+    # formatters = {
+    #   nixfmt = {
+    #     command = "${pkgs.nixfmt}/bin/nixfmt";
+    #     args = [ "$FILENAME" ];
+    #   };
+    #   dart_format = {
+    #     command = "${pkgs.dart}/bin/dart";
+    #     args = [ "format" "$FILENAME" ];
+    #   };
+    # };
+    formattersByFt = {
+      nix = [ "nixfmt" ];
+      dart = [ "dart_format" ];
+      lua = [ "stylua" ];
+      c = [ "clang-format" ];
+      cpp = [ "clang-format" ];
+      python = [ "isort" "ruff_fix" "ruff_format" ];
+      javascript = [[ "prettierd" "prettier" ]];
+      typst = [ "typstfmt" ];
     };
-    formattersByFt = { nix = [ "nixfmt" ]; };
   };
 }
