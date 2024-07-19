9e4050ca9c117c5fafa96ee7a8638c0078ab7343
Runar Fredagsvik <i@runar.ch>
2024-07-19 09:59:39 +0200

    disable lsp progress

--- config/noice.nix
+++ config/noice.nix
@@ -1,18 +1,14 @@
 _:
 
 {
-  # FIXME mini mode doesn't work consistently, and sometimes switches off
-  #  e.g. this will show an error from noice
-  #       :echo "hi
-  #       this will show a message from fidget
-  #       :echo "hi"
   plugins.noice = {
     enable = true;
-    notify.enabled = false;
     messages.view = "mini";
+    notify.view = "mini";
+    lsp.progress.enabled = false;
     presets = {
       bottom_search = true;
-      command_palette = false;
+      command_palette = true;
       long_message_to_split = true;
       inc_rename = true;
       lsp_doc_border = true;
