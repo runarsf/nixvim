36317d27e207c88f94cd4526fab1977d2fea2b0c
Runar Fredagsvik <i@runar.ch>
2025-08-18 15:20:26 +0200

    refactor: autocmds

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -1,13 +1,13 @@
-_: {
-  autoGroups = {
-    CursorLine.clear = true;
-    ForbidReplaceMode.clear = true;
-  };
-
-  autoCmd = [
+{
+  helpers,
+  config,
+  lib,
+  ...
+}: let
+  autoCmds = [
     {
       # Show cursor-line
-      group = "CursorLine";
+      group = "cursorline";
       event = [
         "InsertLeave"
         "WinEnter"
@@ -17,7 +17,7 @@ _: {
     }
     {
       # Hide cursor-line
-      group = "CursorLine";
+      group = "cursorline";
       event = [
         "InsertEnter"
         "WinLeave"
@@ -27,28 +27,123 @@ _: {
     }
     {
       # Check if file has been modified on disk
+      group = "checktime";
       event = [
         "FocusGained"
-        "CursorHold"
+        "TermClose"
+        "TermLeave"
       ];
       pattern = ["*"];
-      command = "checktime";
+      callback = helpers.mkRaw ''
+        function()
+          if vim.o.buftype ~= "nofile" then
+            vim.cmd.checktime()
+          end
+        end
+      '';
     }
     {
       # Get rid of the pesky cmd window
+      group = "forbid_cmdwin";
       event = ["CmdwinEnter"];
       pattern = ["*"];
-      command = "q | call feedkeys(':')";
+      callback = helpers.mkRaw ''
+        function()
+          vim.cmd.q()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":", true, false, true), 'n', false)
+        end
+      '';
     }
     {
       # Forbid replace mode
+      group = "forbid_replace";
       event = [
         "InsertEnter"
         "InsertChange"
       ];
       pattern = ["*"];
-      command = "call ForbidReplace()";
-      group = "ForbidReplaceMode";
+      callback = helpers.mkRaw ''
+        function()
+          if vim.v.insertmode == "r" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Insert>", true, false, true), "n", false)
+          end
+        end
+      '';
+    }
+    {
+      group = "yank_highlight";
+      event = ["TextYankPost"];
+      pattern = ["*"];
+      callback = helpers.mkRaw ''
+        function()
+          (vim.hl or vim.highlight).on_yank({
+            higroup = "CurSearch",
+            timeout = 200,
+          })
+        end
+      '';
+    }
+    {
+      group = "resize_splits";
+      event = ["VimResized"];
+      pattern = ["*"];
+      callback = helpers.mkRaw ''
+        function()
+          local current_tab = vim.fn.tabpagenr()
+          vim.cmd("tabdo wincmd =")
+          vim.cmd("tabnext " .. current_tab)
+        end
+      '';
+    }
+    {
+      group = "close_with_q";
+      event = ["FileType"];
+      pattern = [
+        "PlenaryTestPopup"
+        "checkhealth"
+        "dbout"
+        "gitsigns-blame"
+        "grug-far"
+        "help"
+        "lspinfo"
+        "neotest-output"
+        "neotest-output-panel"
+        "neotest-summary"
+        "notify"
+        "qf"
+        "spectre_panel"
+        "startuptime"
+        "tsplayground"
+      ];
+      callback = helpers.mkRaw ''
+        function(event)
+          vim.bo[event.buf].buflisted = false
+          vim.schedule(function()
+            vim.keymap.set("n", "q", function()
+              vim.cmd("close")
+              pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+            end, {
+              buffer = event.buf,
+              silent = true,
+              desc = "Quit buffer",
+            })
+          end)
+        end
+      '';
+    }
+    {
+      group = "mkdir_p";
+      event = ["BufWritePre"];
+      pattern = ["*"];
+      callback = helpers.mkRaw ''
+        function(event)
+          if event.match:match("^%w%w+:[\\/][\\/]") then
+            return
+          end
+          local file = vim.uv.fs_realpath(event.match) or event.match
+          vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+        end
+      '';
     }
     # { # Disable automatic commenting on newline
     #   event = [ "FileType" "BufNewFile" "BufRead" ];
@@ -57,11 +152,9 @@ _: {
     # }
   ];
 
-  extraConfigVim = ''
-    function ForbidReplace()
-      if v:insertmode isnot# 'i'
-        call feedkeys("\<Insert>", "n")
-      endif
-    endfunction
-  '';
+  # Dynamically generate autoGroups from the groups used in autoCmds
+  usedGroups = lib.unique (map (cmd: cmd.group) autoCmds);
+in {
+  autoGroups = lib.genAttrs usedGroups (name: {clear = true;});
+  autoCmd = autoCmds;
 }
