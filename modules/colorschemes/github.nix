2dbba72c02d34361ce8cb3ed7e2f49f2bf3dd76f
Runar Fredagsvik <i@runar.ch>
2025-08-19 00:21:57 +0200

    fix: remove empty module, change colorscheme name

--- modules/colorschemes/github.nix
+++ modules/colorschemes/github.nix
@@ -5,7 +5,7 @@
 }:
 lib.utils.mkColorschemeModule config "github" {
   # github_dark_default
-  colorschemes.github = {
+  colorschemes.github-theme = {
     enable = true;
     settings = {
       transparent = lib.mkIf config.modules.colorschemes.transparent true;
